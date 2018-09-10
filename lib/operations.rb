f2d4e1001c97cdacca91515a93444595524bf9ab
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 10:20:56 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
 	if speed < 40 || speed > 60
-		true
+		"true"
 	else
-		false
+		"false"
 	end
 end
 
 
 
 def not_safe?(speed)
-	if speed < 40 || speed > 60 ? true : false
+	if speed < 40 || speed > 60 ? "true" : "false"
 	end
 end
